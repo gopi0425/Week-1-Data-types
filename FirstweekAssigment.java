db60986b73ff10129c03baa4f0c811186784bdf8
Gopinath <137993085+gopi0425@users.noreply.github.com>
2025-04-27 08:59:47 +0530

    Add files via upload

---
+++ FirstweekAssigment.java
@@ -0,0 +1,26 @@
+package learn.selenium;
+
+public class FirstweekAssigment {
+
+	public static void main(String[] args) {
+		
+		
+		float Time=5212347;
+		System.out.println(Time);
+		
+		String location="chennai";
+		System.out.println(location);
+		
+		boolean result=false;
+		System.out.println(result);
+		
+		int age=28;
+		System.out.println(age);
+		
+		char gender='M';
+		System.out.println(gender);
+			
+
+	}
+
+}
